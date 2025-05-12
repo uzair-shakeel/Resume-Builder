f1003dafb6698c1348dd4a48892fbe7db3571f0b
uzair-shakeel <uzairshakeel00000@gmail.com>
2025-05-12 19:33:25 +0500

    deployment fix

--- app/api/cover-letter/load/route.ts
+++ app/api/cover-letter/load/route.ts
@@ -4,6 +4,8 @@ import { authOptions } from "@/lib/auth";
 import dbConnect from "@/lib/dbConnect";
 import CoverLetter from "@/models/CoverLetter";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(req: Request) {
   try {
     const session = await getServerSession(authOptions);
